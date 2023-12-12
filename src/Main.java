c0079491503768db6236858dbf2d0d2e548cd913
Andreas Olausson <42862838+Olauss0n@users.noreply.github.com>
2023-12-12 19:04:18 +0100

    Advent of Code 2023 Dec 11 - Two stars solution

--- src/Main.java
+++ src/Main.java
@@ -5,33 +5,35 @@ import day.y2023.Day05;
 import day.y2023.Day06;
 import day.y2023.Day07;
 import day.y2023.Day09;
+import day.y2023.Day11;
 
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
         long start = System.currentTimeMillis();
-        if (args.length == 1) {
-            int argument = Arrays.stream(args).map(Integer::parseInt).findFirst().orElseThrow();
+        int argument = Arrays.stream(args).map(Integer::parseInt).findFirst().orElseThrow();
 
-            System.out.printf("Today's date is %s December and the answer is: %n", argument);
+        System.out.printf("Today's date is %s December and the answer is: %n", argument);
 
-            if (argument == 1) {
-                Day01.run();
-            } else if (argument == 2) {
-                Day02.run();
-            } else if (argument == 4) {
-                Day04.run();
-            } else if (argument == 5) {
-                Day05.run();
-            } else if (argument == 6) {
-                Day06.run();
-            } else if (argument == 7) {
-                Day07.run();
-            } else if (argument == 9) {
-                Day09.run();
-            }
+        if (argument == 1) {
+            Day01.run();
+        } else if (argument == 2) {
+            Day02.run();
+        } else if (argument == 4) {
+            Day04.run();
+        } else if (argument == 5) {
+            Day05.run();
+        } else if (argument == 6) {
+            Day06.run();
+        } else if (argument == 7) {
+            Day07.run();
+        } else if (argument == 9) {
+            Day09.run();
+        } else if (argument == 11) {
+            Day11.run();
         }
+
         System.out.println("Evaluation took: " + (System.currentTimeMillis() - start) + " ms");
     }
 }
