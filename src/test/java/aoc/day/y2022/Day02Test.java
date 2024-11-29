d394aa33e7f179ec797e6a2898ea698667b8d7c6
Andreas Olausson <42862838+Olauss0n@users.noreply.github.com>
2024-11-29 18:38:46 +0100

    AoC 2022 Day 2, add lombok

---
+++ src/test/java/aoc/day/y2022/Day02Test.java
@@ -0,0 +1,11 @@
+package aoc.day.y2022;
+
+import aoc.day.AdventOfCodeBaseTest;
+import aoc.util.AdventOfCodeSolver;
+
+public class Day02Test extends AdventOfCodeBaseTest {
+    @Override
+    protected AdventOfCodeSolver getSolver() {
+        return new Day02();
+    }
+}
