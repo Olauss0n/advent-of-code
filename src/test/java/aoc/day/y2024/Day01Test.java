de9c7f36ff05a7e546c9e23f5572bb48c9be4073
Andreas Olausson <42862838+Olauss0n@users.noreply.github.com>
2024-11-29 10:52:50 +0100

    Create abstract base test for all other test classes to inherit from. Create dummy Day01 2024

---
+++ src/test/java/aoc/day/y2024/Day01Test.java
@@ -0,0 +1,12 @@
+package aoc.day.y2024;
+
+import aoc.day.AdventOfCodeBaseTest;
+import aoc.util.AdventOfCodeSolver;
+
+public class Day01Test extends AdventOfCodeBaseTest {
+
+    @Override
+    protected AdventOfCodeSolver getSolver() {
+        return new Day01();
+    }
+}
