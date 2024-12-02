1764b236bc87aa6e49acf5069004994893fd16d7
Andreas Olausson <42862838+Olauss0n@users.noreply.github.com>
2024-12-02 09:33:05 +0100

    AoC 2024 Day 2 - Two stars solution

---
+++ src/main/java/aoc/day/y2024/Day02.java
@@ -0,0 +1,69 @@
+package aoc.day.y2024;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
+
+import aoc.util.AdventOfCodeSolver;
+import aoc.util.Reader;
+
+public class Day02 implements AdventOfCodeSolver {
+
+    @Override
+    public Object solvePartOne() {
+        List<String> inputList = Reader.readInputAsList(this.getClass());
+        int counter = 0;
+        for (String line : inputList) {
+            int[] intList = Arrays.stream(line.split("\\s+"))
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
+            if (increasingOrDecreasing(intList)) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+
+    @Override
+    public Object solvePartTwo() {
+        List<String> inputList = Reader.readInputAsList(this.getClass());
+        int counter = 0;
+        for (String line : inputList) {
+            int[] intList = Arrays.stream(line.split("\\s+"))
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
+            if (increasingOrDecreasingWithFaultTolerance(intList)) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+
+    private boolean increasingOrDecreasing(int[] intList) {
+        boolean increasing = IntStream.range(1, intList.length)
+                .allMatch(i -> intList[i] > intList[i - 1] && intList[i] - intList[i - 1] <= 3);
+        boolean decreasing = IntStream.range(1, intList.length)
+                .allMatch(i -> intList[i] < intList[i - 1] && intList[i - 1] - intList[i] <= 3);
+
+        return increasing || decreasing;
+    }
+
+    private boolean increasingOrDecreasingWithFaultTolerance(int[] intList) {
+        if (increasingOrDecreasing(intList)) {
+            return true;
+        }
+
+        for (int i = 0; i < intList.length; i++) {
+            int index = i;
+            int[] reducedList = IntStream.range(0, intList.length)
+                    .filter(j -> j != index)
+                    .map(j -> intList[j])
+                    .toArray();
+
+            if (increasingOrDecreasing(reducedList)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
