15708f40937fbe4b50125b010c505fdfe396b5fd
Andreas Olausson <42862838+Olauss0n@users.noreply.github.com>
2024-12-03 22:30:58 +0100

    AoC 2022 Day 3 - Two stars solution

---
+++ src/main/java/aoc/day/y2022/Day03.java
@@ -0,0 +1,69 @@
+package aoc.day.y2022;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import aoc.util.AdventOfCodeSolver;
+import aoc.util.Reader;
+
+public class Day03 implements AdventOfCodeSolver {
+    @Override
+    public Object solvePartOne() {
+        List<String> inputList = Reader.readInputAsList(this.getClass());
+        int result = 0;
+        for (String line : inputList) {
+            String firstHalf = line.substring(0, line.length() / 2);
+            String secondHalf = line.substring(line.length() / 2);
+
+            Set<Character> firstHalfSet =
+                    firstHalf.chars().mapToObj(c -> (char) c).collect(Collectors.toSet());
+            result += calculateCommonCharacter(firstHalfSet, secondHalf);
+        }
+        return result;
+    }
+
+    @Override
+    public Object solvePartTwo() {
+        List<List<String>> inputMatrix = Reader.readInputAsMatrix(this.getClass(), 3);
+
+        int result = 0;
+        for (List<String> list : inputMatrix) {
+            String first = list.get(0);
+            String second = list.get(1);
+            String third = list.get(2);
+
+            HashSet<Character> firstThirdSet = new HashSet<>();
+            for (char c : first.toCharArray()) {
+                firstThirdSet.add(c);
+            }
+
+            HashSet<Character> secondThirdSet = new HashSet<>();
+            for (char c : second.toCharArray()) {
+                if (firstThirdSet.contains(c)) {
+                    secondThirdSet.add(c);
+                }
+            }
+            result += calculateCommonCharacter(secondThirdSet, third);
+        }
+        return result;
+    }
+
+    private int calculateCommonCharacter(Set<Character> set, String string) {
+        return string.chars()
+                .mapToObj(character -> (char) character)
+                .filter(set::contains)
+                .findFirst()
+                .map(c -> {
+                    if (Character.isLowerCase(c)) {
+                        return c - 'a' + 1;
+                    }
+                    if (Character.isUpperCase(c)) {
+                        return c - 'A' + 27;
+                    }
+                    return 0;
+                })
+                .orElseThrow();
+    }
+}
---
+++ src/test/java/aoc/day/y2022/Day03Test.java
@@ -0,0 +1,11 @@
+package aoc.day.y2022;
+
+import aoc.day.AdventOfCodeBaseTest;
+import aoc.util.AdventOfCodeSolver;
+
+public class Day03Test extends AdventOfCodeBaseTest {
+    @Override
+    protected AdventOfCodeSolver getSolver() {
+        return new Day03();
+    }
+}
