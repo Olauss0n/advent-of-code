23ae5dffefbce6f140f1e1077534f0a4810ca473
Andreas Olausson <42862838+Olauss0n@users.noreply.github.com>
2024-12-07 21:42:26 +0100

    AoC 2024 Day 7 - Two stars solution

---
+++ src/main/java/aoc/day/y2024/Day07.java
@@ -0,0 +1,56 @@
+package aoc.day.y2024;
+
+import java.util.Arrays;
+import java.util.List;
+
+import aoc.util.AdventOfCodeSolver;
+import aoc.util.Reader;
+
+public class Day07 implements AdventOfCodeSolver {
+    @Override
+    public Object solvePartOne() {
+        return commonPart(false);
+    }
+
+    @Override
+    public Object solvePartTwo() {
+        return commonPart(true);
+    }
+
+    private Object commonPart(boolean isPartTwo) {
+        List<String> inputList = Reader.readInputAsList(this.getClass());
+
+        long total = 0;
+        for (String line : inputList) {
+            String[] split = line.split(": ");
+            long target = Long.parseLong(split[0]);
+            List<Long> numbers =
+                    Arrays.stream(split[1].split(" ")).map(Long::parseLong).toList();
+            if (canSolve(target, numbers, isPartTwo)) {
+                total += target;
+            }
+        }
+        return total;
+    }
+
+    private boolean canSolve(long target, List<Long> numbers, boolean isPartTwo) {
+        List<Long> subList = numbers.subList(0, numbers.size() - 1);
+        if (numbers.size() == 1) {
+            return target == numbers.getFirst();
+        }
+        if (target % numbers.getLast() == 0 && canSolve(target / numbers.getLast(), subList, isPartTwo)) {
+            return true;
+        }
+        if (target > numbers.getLast() && canSolve(target - numbers.getLast(), subList, isPartTwo)) {
+            return true;
+        }
+        if (isPartTwo) {
+            String tar = String.valueOf(target);
+            String last = String.valueOf(numbers.getLast());
+            return tar.length() > last.length()
+                    && tar.endsWith(last)
+                    && canSolve(Long.parseLong(tar.substring(0, tar.length() - last.length())), subList, true);
+        }
+        return false;
+    }
+}
