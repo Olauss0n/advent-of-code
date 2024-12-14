6bd2e19ad36f83f25686cdf707a6685be956687f
Andreas Olausson <42862838+Olauss0n@users.noreply.github.com>
2024-12-14 10:10:11 +0100

    AoC 2024 Day 12 - One star solution

---
+++ src/main/java/aoc/day/y2024/Day12.java
@@ -0,0 +1,100 @@
+package aoc.day.y2024;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map.Entry;
+import java.util.Set;
+
+import aoc.util.AdventOfCodeSolver;
+import aoc.util.Converter;
+import aoc.util.Reader;
+
+public class Day12 implements AdventOfCodeSolver {
+    @Override
+    public Object solvePartOne() {
+        List<String> inputList = Reader.readInputAsList(this.getClass());
+        String[][] matrix = Converter.convertListInputToStringMatrix(inputList);
+
+        List<HashMap<String, Set<Position>>> arrangements = new ArrayList<>();
+        HashMap<Position, Boolean> visited = new HashMap<>();
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                String type = matrix[i][j];
+                Position position = new Position(i, j);
+
+                HashMap<String, Set<Position>> arrangement = new HashMap<>();
+
+                if (!visited.containsKey(position)) {
+                    Set<Position> positions = checkAdjacentPositions(matrix, position, visited, new HashSet<>());
+                    positions.add(position);
+                    arrangement.put(type, positions);
+                    arrangements.add(arrangement);
+                }
+            }
+        }
+        int result = 0;
+        for (HashMap<String, Set<Position>> arrangement : arrangements) {
+            for (Entry<String, Set<Position>> entrySet : arrangement.entrySet()) {
+                Set<Position> positions = entrySet.getValue();
+                int value = 4 * positions.size();
+                for (Position position : positions) {
+                    for (Direction direction : Direction.values()) {
+                        Position newPos = position.move(direction);
+                        if (isWithinBounds(matrix, newPos) && positions.contains(newPos)) {
+                            value -= 1;
+                        }
+                    }
+                }
+                result += value * positions.size();
+            }
+        }
+        return result;
+    }
+
+    private Set<Position> checkAdjacentPositions(
+            String[][] matrix, Position pos, HashMap<Position, Boolean> visited, Set<Position> set) {
+        if (visited.containsKey(pos)) {
+            return set;
+        }
+        visited.put(pos, true);
+        set.add(pos);
+        for (Direction direction : Direction.values()) {
+            Position newPos = pos.move(direction);
+            if (isWithinBounds(matrix, newPos) && matrix[newPos.row][newPos.col].equals(matrix[pos.row][pos.col])) {
+                set.add(newPos);
+                set = checkAdjacentPositions(matrix, newPos, visited, set);
+            }
+        }
+        return set;
+    }
+
+    @Override
+    public Object solvePartTwo() {
+        return null;
+    }
+
+    private boolean isWithinBounds(String[][] matrix, Position pos) {
+        return (pos.col >= 0 && pos.row >= 0 && pos.row < matrix.length && pos.col < matrix[0].length);
+    }
+
+    private record Position(Integer row, Integer col) {
+
+        private Position move(Direction direction) {
+            return switch (direction) {
+                case UP -> new Position(row - 1, col);
+                case RIGHT -> new Position(row, col + 1);
+                case DOWN -> new Position(row + 1, col);
+                case LEFT -> new Position(row, col - 1);
+            };
+        }
+    }
+
+    private enum Direction {
+        UP,
+        DOWN,
+        LEFT,
+        RIGHT
+    }
+}
---
+++ src/test/java/aoc/day/y2024/Day12Test.java
@@ -0,0 +1,11 @@
+package aoc.day.y2024;
+
+import aoc.day.AdventOfCodeBaseTest;
+import aoc.util.AdventOfCodeSolver;
+
+public class Day12Test extends AdventOfCodeBaseTest {
+    @Override
+    protected AdventOfCodeSolver getSolver() {
+        return new Day12();
+    }
+}
