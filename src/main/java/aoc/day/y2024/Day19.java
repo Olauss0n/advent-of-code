ab17a176e385f06a9841310fd423af0fdb5ae666
Andreas Olausson <42862838+Olauss0n@users.noreply.github.com>
2024-12-22 21:38:05 +0100

    AoC 2024 Day 19 - Two stars solution

---
+++ src/main/java/aoc/day/y2024/Day19.java
@@ -0,0 +1,53 @@
+package aoc.day.y2024;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.stream.Stream;
+
+import aoc.util.AdventOfCodeSolver;
+import aoc.util.Converter;
+
+public class Day19 implements AdventOfCodeSolver {
+    @Override
+    public Object solvePartOne(String input) {
+        return commonPart(input).count();
+    }
+
+    @Override
+    public Object solvePartTwo(String input) {
+        return commonPart(input).reduce(Long::sum).orElseThrow();
+    }
+
+    private final HashMap<String, Long> cache = new HashMap<>();
+
+    private Stream<Long> commonPart(String input) {
+        List<String> inputList = Converter.convertInputToList(input).stream()
+                .filter(element -> !element.isBlank())
+                .toList();
+        List<String> towelPatterns =
+                Arrays.stream(inputList.getFirst().split(", ")).toList();
+        List<String> designs = inputList.subList(1, inputList.size());
+
+        for (String design : designs) {
+            countPossible(design, towelPatterns);
+        }
+        return designs.stream().map(cache::get).filter(value -> value > 0);
+    }
+
+    private long countPossible(String design, List<String> patterns) {
+        if (cache.containsKey(design)) {
+            return cache.get(design);
+        }
+        long count = 0;
+        for (String pattern : patterns) {
+            if (design.equals(pattern)) {
+                count += 1;
+            } else if (design.startsWith(pattern)) {
+                count += countPossible(design.substring(pattern.length()), patterns);
+            }
+        }
+        cache.put(design, count);
+        return count;
+    }
+}
---
+++ src/test/java/aoc/day/y2024/Day19Test.java
@@ -0,0 +1,21 @@
+package aoc.day.y2024;
+
+import aoc.day.AdventOfCodeBaseTest;
+import aoc.util.AdventOfCodeSolver;
+
+public class Day19Test extends AdventOfCodeBaseTest {
+    @Override
+    protected AdventOfCodeSolver getSolver() {
+        return new Day19();
+    }
+
+    @Override
+    protected Object getExampleSolutionPartOne() {
+        return 6;
+    }
+
+    @Override
+    protected Object getExampleSolutionPartTwo() {
+        return 16;
+    }
+}
