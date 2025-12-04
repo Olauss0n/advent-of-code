a4f1f9b6cda3b9022ae486737f75e77034048f79
Andreas Olausson <42862838+Olauss0n@users.noreply.github.com>
2025-12-04 08:55:42 +0100

    AoC 2025 Day 3 - Two stars solution

---
+++ src/main/java/aoc/day/y2025/Day03.java
@@ -0,0 +1,58 @@
+package aoc.day.y2025;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import aoc.util.AdventOfCodeSolver;
+import aoc.util.Converter;
+
+public class Day03 implements AdventOfCodeSolver {
+    @Override
+    public Object solvePartOne(String input, boolean isExample) {
+        List<String> inputList = Converter.convertInputToList(input);
+        long result = 0;
+        for (String line : inputList) {
+            int digitsToPick = 2;
+            result += findHighestDigits(line, digitsToPick);
+        }
+        return result;
+    }
+
+    @Override
+    public Object solvePartTwo(String input, boolean isExample) {
+        List<String> inputList = Converter.convertInputToList(input);
+        long result = 0;
+        for (String line : inputList) {
+            int digitsToPick = 12;
+            result += findHighestDigits(line, digitsToPick);
+        }
+        return result;
+    }
+
+    private Long findHighestDigits(String line, int digitsToPick) {
+        List<Long> digits = new ArrayList<>();
+        int startIndex = 0;
+        for (int remaining = digitsToPick - 1; remaining >= 0; remaining--) {
+            Joltage joltage = findHighestDigit(line, startIndex, remaining);
+            digits.add(joltage.value());
+            startIndex = joltage.index();
+        }
+        return Long.parseLong(digits.stream().map(String::valueOf).collect(Collectors.joining()));
+    }
+
+    private Joltage findHighestDigit(String line, int startIndex, int endOffset) {
+        long value = -1;
+        int index = -1;
+        for (int i = startIndex; i < line.length() - endOffset; i++) {
+            long currentDigit = Long.parseLong(line.substring(i, i + 1));
+            if (currentDigit > value) {
+                value = currentDigit;
+                index = i;
+            }
+        }
+        return new Joltage(value, index + 1);
+    }
+
+    private record Joltage(long value, int index) {}
+}
---
+++ src/test/java/aoc/day/y2025/Day03Test.java
@@ -0,0 +1,21 @@
+package aoc.day.y2025;
+
+import aoc.day.AdventOfCodeBaseTest;
+import aoc.util.AdventOfCodeSolver;
+
+public class Day03Test extends AdventOfCodeBaseTest {
+    @Override
+    protected AdventOfCodeSolver getSolver() {
+        return new Day03();
+    }
+
+    @Override
+    protected Object getExampleSolutionPartOne() {
+        return 357;
+    }
+
+    @Override
+    protected Object getExampleSolutionPartTwo() {
+        return 3121910778619L;
+    }
+}
