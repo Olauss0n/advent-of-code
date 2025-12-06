892087fdf948813e8c3cf0ed862c6ccd2a78a163
Andreas Olausson <42862838+Olauss0n@users.noreply.github.com>
2025-12-06 14:54:52 +0100

    Simplify main method to run without arguments

--- src/main/java/aoc/Main.java
+++ src/main/java/aoc/Main.java
@@ -1,26 +1,21 @@
 package aoc;
 
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
+import java.time.LocalDate;
 
 import aoc.util.InputFetcher;
 
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
         long start = System.currentTimeMillis();
-        List<Integer> arguments = Arrays.stream(args).map(Integer::parseInt).toList();
-        int year = arguments.getFirst();
-        int day = arguments.getLast();
-        if (year > 2035 || year < 2015) {
-            System.out.println("Invalid year: " + year);
-            return;
+        int year, day;
+        if (args.length == 0) {
+            year = LocalDate.now().getYear();
+            day = LocalDate.now().getDayOfMonth();
+        } else {
+            year = Integer.parseInt(args[0]);
+            day = Integer.parseInt(args[1]);
         }
-        if (day < 1 || day > 31) {
-            System.out.println("Invalid day: " + day);
-            return;
-        }
-
         InputFetcher.fetchInput(year, day);
 
         System.out.println("Evaluation took: " + (System.currentTimeMillis() - start) + " ms");
