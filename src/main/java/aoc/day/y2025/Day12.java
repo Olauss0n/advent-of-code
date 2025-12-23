41594453f47d870eff2aa827d9627ebc1626f24e
Andreas Olausson <42862838+Olauss0n@users.noreply.github.com>
2025-12-23 11:31:23 +0100

    AoC 2025 Day 12 - One star solution

---
+++ src/main/java/aoc/day/y2025/Day12.java
@@ -0,0 +1,40 @@
+package aoc.day.y2025;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import aoc.util.AdventOfCodeSolver;
+
+public class Day12 implements AdventOfCodeSolver {
+    @Override
+    public Object solvePartOne(String input, boolean isExample) {
+        List<String> inputList = Arrays.asList(input.split("\n\n"));
+        String[] regions = inputList.getLast().split("\n");
+        Pattern pattern = Pattern.compile("\\d+");
+        int result = 0;
+        for (String region : regions) {
+            Matcher matcher = pattern.matcher(region);
+            List<Integer> numbers = new ArrayList<>();
+            while (matcher.find()) {
+                numbers.add(Integer.parseInt(matcher.group()));
+            }
+            int width = numbers.getFirst();
+            int length = numbers.get(1);
+            int quantity = numbers.subList(2, numbers.size()).stream()
+                    .reduce(Integer::sum)
+                    .orElseThrow();
+            if (width / 3 * length / 3 >= quantity) {
+                result += 1;
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public Object solvePartTwo(String input, boolean isExample) {
+        return null;
+    }
+}
---
+++ src/test/java/aoc/day/y2025/Day12Test.java
@@ -0,0 +1,22 @@
+package aoc.day.y2025;
+
+import aoc.day.AdventOfCodeBaseTest;
+import aoc.util.AdventOfCodeSolver;
+import aoc.util.exceptions.NotImplementedException;
+
+public class Day12Test extends AdventOfCodeBaseTest {
+    @Override
+    protected AdventOfCodeSolver getSolver() {
+        return new Day12();
+    }
+
+    @Override
+    protected Object getExampleSolutionPartOne() {
+        return 2;
+    }
+
+    @Override
+    protected Object getExampleSolutionPartTwo() {
+        throw new NotImplementedException();
+    }
+}
